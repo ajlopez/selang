c22dfff0204fd73536002adf3f61fc71f8c6c365
ajlopez <webmaster@ajlopez.com>
2019-03-24 11:22:30 -0300

    Parse subtract

--- test/parser.js
+++ test/parser.js
@@ -63,6 +63,14 @@ exports['parse add'] = function (test) {
     parseBinary(test, 'foo + bar', [ 'foo', '+', 'bar' ]);
 };
 
+exports['parse subtract'] = function (test) {
+    parseBinary(test, '42 - 0', [ 42, '-', 0] );
+    parseBinary(test, '1 - 2', [ 1, '-', 2 ]);
+    parseBinary(test, '42 - foo', [ 42, '-', 'foo' ]);
+    parseBinary(test, 'foo - 42', [ 'foo', '-', 42 ]);
+    parseBinary(test, 'foo - bar', [ 'foo', '-', 'bar' ]);
+};
+
 function parseBinary(test, text, expected) {
     const node = parser.parse('expression', text);
     const obj = toObj(expected);
