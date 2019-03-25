a359cde6e6c62198df0c2f478dd5eac5c0afb0ba
ajlopez <webmaster@ajlopez.com>
2019-03-25 18:14:12 -0300

    Parse continue command

--- test/cmds.js
+++ test/cmds.js
@@ -8,3 +8,10 @@ exports['parse break command'] = function (test) {
     test.equal(result.ntype(), 'break');
 };
 
+exports['parse continue command'] = function (test) {
+    const result = parser.parse('command', 'continue;');
+    
+    test.ok(result);
+    test.equal(result.ntype(), 'continue');
+};
+
