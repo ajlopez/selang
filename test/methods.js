da9a648c8e9751959b09514c4616964bc33d22bd
ajlopez <webmaster@ajlopez.com>
2019-03-25 19:40:14 -0300

    Parse void empty method

---
+++ test/methods.js
@@ -0,0 +1,40 @@
+
+const parser = require('../lib/parser');
+
+exports['parse empty public void method'] = function (test) {
+    const result = parser.parse('method', 'void foo() {}');
+    
+    match(test, result, {
+        ntype: 'method',
+        visibility: 'private',
+        type: 'void',
+        arguments: [],
+        body: {
+            ntype: 'sequence',
+            nodes: []
+        }
+    });
+};
+
+function match(test, node, obj) {
+    test.ok(node);
+    
+    for (var n in obj) {
+        test.ok(node[n]);
+        
+        let value;
+        
+        if (typeof node[n] === 'function')
+            value = node[n]();
+        else
+            value = node[n];
+        
+        const expected = obj[n];
+        
+        if (value != null && typeof value === 'object')
+            match(test, value, expected);
+        else
+            test.strictEqual(value, expected);
+    }
+}
+
