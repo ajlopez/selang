2da61fb57e5640907a54a0bf5efc8e43de75e0ff
ajlopez <webmaster@ajlopez.com>
2019-04-02 14:22:19 -0300

    Improve deploy in sample

--- samples/compile/deploy.js
+++ samples/compile/deploy.js
@@ -5,12 +5,8 @@ const fs = require('fs');
 
 const code = fs.readFileSync('Counter.sel').toString();
 
-console.log(code);
-
+console.log('compiling contract...');
 const result = selang.compile(code);
-
-console.log();
-console.log(result);
 
 const host = rskapi.host('http://localhost:8545');
 
@@ -23,18 +19,17 @@ const host = rskapi.host('http://localhost:8545');
         gasPrice: 0,
         data: result
     }
-    
+
+    console.log('deploying instance...');
     const txhash = await host.sendTransaction(tx);
     let txrec = await host.getTransactionReceiptByHash(txhash);
     
     while (!txrec)
         txrec = await host.getTransactionReceiptByHash(txhash);
-    
-    console.dir(txrec);
+
+    console.log('deployed at', txrec.contractAddress);
     
     const runcode = await host.getCode(txrec.contractAddress);
-    
-    console.log(runcode);
     
     const tx2 = {
         from: accounts[0],
@@ -46,7 +41,7 @@ const host = rskapi.host('http://localhost:8545');
     
     const counter = await host.callTransaction(tx2);
     
-    console.log(parseInt(counter));
+    console.log('counter', parseInt(counter));
     
     const tx3 = {
         from: accounts[0],
@@ -56,16 +51,36 @@ const host = rskapi.host('http://localhost:8545');
         data: 'd09de08a'
     }
 
+    console.log('increment...');
+    
     const txhash3 = await host.sendTransaction(tx3);
     let txrec3 = await host.getTransactionReceiptByHash(txhash3);
     
     while (!txrec3)
         txrec3 = await host.getTransactionReceiptByHash(txhash3);
 
-    console.log(txrec3);
-    
     const counter2 = await host.callTransaction(tx2);
     
-    console.log(parseInt(counter2));
+    console.log('counter', parseInt(counter2));
+    
+    console.log('add 40...');
+    
+    const tx4 = {
+        from: accounts[0],
+        to: txrec.contractAddress,
+        gas: 6000000,
+        gasPrice: 0,
+        data: '1003e2d20000000000000000000000000000000000000000000000000000000000000029'
+    }
+
+    const txhash4 = await host.sendTransaction(tx4);
+    let txrec4 = await host.getTransactionReceiptByHash(txhash4);
+    
+    while (!txrec4)
+        txrec4 = await host.getTransactionReceiptByHash(txhash4);
+
+    const counter3 = await host.callTransaction(tx2);
+    
+    console.log('counter', parseInt(counter3));
 })();
 
