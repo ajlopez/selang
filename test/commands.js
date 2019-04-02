c78b0e0d3d89d8edb09046d753131a63ad813b1c
ajlopez <webmaster@ajlopez.com>
2019-04-02 14:59:17 -0300

    Parse if commands

---
+++ test/commands.js
@@ -0,0 +1,117 @@
+
+const parser = require('../lib/parser');
+
+exports['parse if command'] = function (test) {
+    const result = parser.parse('command', 'if (k < 10) k = k + 1;');
+
+    match(test, result, {
+        ntype: 'conditional',
+        condition: {
+            ntype: 'binary',
+            operator: '<',
+            left: {
+                ntype: 'name',
+                name: 'k'
+            },
+            right: {
+                ntype: 'constant',
+                value: 10
+            }
+        },
+        then: {
+            ntype: 'assign',
+            lefthand: {
+                ntype: 'name',
+                name: 'k'
+            },
+            expression: {
+                ntype: 'binary',
+                operator: '+',
+                left: {
+                    ntype: 'name',
+                    name: 'k'
+                },
+                right: {
+                    ntype: 'constant',
+                    value: 1
+                }
+            }
+        }
+    });
+};
+
+exports['parse if command with else'] = function (test) {
+    const result = parser.parse('command', 'if (k > 10) k = k - 1; else k = 0');
+
+    match(test, result, {
+        ntype: 'conditional',
+        condition: {
+            ntype: 'binary',
+            operator: '>',
+            left: {
+                ntype: 'name',
+                name: 'k'
+            },
+            right: {
+                ntype: 'constant',
+                value: 10
+            }
+        },
+        then: {
+            ntype: 'assign',
+            lefthand: {
+                ntype: 'name',
+                name: 'k'
+            },
+            expression: {
+                ntype: 'binary',
+                operator: '-',
+                left: {
+                    ntype: 'name',
+                    name: 'k'
+                },
+                right: {
+                    ntype: 'constant',
+                    value: 1
+                }
+            }
+        },
+        else: {
+            ntype: 'assign',
+            lefthand: {
+                ntype: 'name',
+                name: 'k'
+            },
+            expression: {
+                ntype: 'constant',
+                value: 0
+            }
+        }
+    });
+};
+
+function match(test, node, obj) {
+    test.ok(node);
+    
+    for (var n in obj) {
+        test.ok(node[n]);
+        
+        let value;
+        
+        if (typeof node[n] === 'function')
+            value = node[n]();
+        else
+            value = node[n];
+        
+        const expected = obj[n];
+        
+        if (expected === value)
+            return;
+        
+        if (value != null && typeof value === 'object')
+            match(test, value, expected);
+        else
+            test.strictEqual(value, expected);
+    }
+}
+
